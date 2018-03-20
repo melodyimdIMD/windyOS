1bd9be358fff3d47e809788b1941d9bb11bc0c01
ljq <ljq_dp@163.com>
2018-03-20 16:56:27 +0800

    hh

---
+++ src/kernel/protable.h
@@ -0,0 +1,15 @@
+#ifndef PROTABLE_H
+#define PROTABLE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#define USART_UDRE_vect  USART0_UDRE_vect
+#define USART_RXC_vect   USART0_RX_vect
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PORTMACRO_H */
